9116ba24af6b3beda7a942baf28f3ebbafb740a1
linomeyer <linomeyer02@gmail.com>
2018-12-07 13:30:18 +0100

    create LinkedList

---
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -0,0 +1,36 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+typedef struct struElement {
+	char vorname[40];
+	char nachname[40];
+	int jahrgang;
+	struElement* pNext;
+} struElement;
+
+struElement* create(int amount) {
+	struElement* pStart = NULL;
+	struElement* pLast = NULL;
+
+	for (int i = 0; i < amount; i++) {
+		struElement* pNew = (struElement*)malloc(sizeof(struElement)); // malloc == new
+		pNew->pNext = NULL;
+		strcpy_s(pNew->vorname, "A");
+		strcpy_s(pNew->nachname, "B");
+		pNew->jahrgang = 1925;
+		if (pStart == NULL) pStart = pNew;
+		if (pLast != NULL) pLast->pNext = pNew;
+		pLast = pNew;
+	}
+
+	return pStart;
+}
+
+
+
+void main() {
+	struElement* pStart = create(5);
+
+	system("pause");
+}
