873446b020f011625dc74b330730a509d388100a
linomeyer <linomeyer02@gmail.com>
2018-12-14 13:38:33 +0100

    delete List
    Die Funktion ist implementiert, die scanf_s abfrage geht aber irgendwie nicht -.-

--- LinkedList/LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -4,9 +4,9 @@
 #include "time.h"
 
 typedef struct Person {
-	char vorname[40];
-	char nachname[40];
-	int jahrgang;
+	char firstname[40];
+	char lastname[40];
+	int year;
 	struct Person* pNext;
 } struPerson;
 
@@ -25,11 +25,11 @@ struPerson* create(int amount) {
 	for (int i = 0; i < amount; i++) {
 		struPerson* pNew = (struPerson*)malloc(sizeof(struPerson)); // malloc == new
 		pNew->pNext = NULL;
-		pNew->vorname[0] = randomName();
-		pNew->vorname[1] = '\0';
-		pNew->nachname[0] = randomName();
-		pNew->nachname[1] = '\0';
-		pNew->jahrgang = randomYear();
+		pNew->firstname[0] = randomName();
+		pNew->firstname[1] = '\0';
+		pNew->lastname[0] = randomName();
+		pNew->lastname[1] = '\0';
+		pNew->year = randomYear();
 		if (pStart == NULL) pStart = pNew;
 		if (pLast != NULL) pLast->pNext = pNew;
 		pLast = pNew;
@@ -37,15 +37,42 @@ struPerson* create(int amount) {
 	return pStart;
 }
 
+void deleteList(struPerson* pStart) {
+	struPerson* pCurrentPerson = pStart;
+	struPerson* pNextPerson;
+
+	while (pCurrentPerson != NULL) {
+		// pNextPerson zeigt auf nächstes Personenelement
+		pNextPerson = pCurrentPerson->pNext;
+		// momentanes Elements löschen
+		free(pCurrentPerson);
+		pCurrentPerson = pNextPerson;
+	}
+}
+
 void printList(struPerson* pStart) {
 	for (struPerson* pOutput = pStart; pOutput != NULL; pOutput = pOutput->pNext) {
-		printf("Vorname: %c\nNachname: %c\nJahrgang: %d\n", pOutput->vorname[0], pOutput->nachname[0], pOutput->jahrgang);
+		printf("Vorname: %c\nNachname: %c\nJahrgang: %d\n", pOutput->firstname[0], pOutput->lastname[0], pOutput->year);
 	}
 }
 
 void main() {
 	srand((unsigned)time(NULL));
-	struPerson* pStart = create(5);
+	// Liste erstellen
+	printf("Geben Sie die Anzahl Elemente die Sie erstellen moechten ein: ");
+	int createElements;
+	scanf_s("%d", &createElements);
+	struPerson* pStart = create(createElements);
 	printList(pStart);
+
+	deleteList(pStart);
+	// Liste löschen
+	//printf("Geben Sie \"j\" ein wenn Sie die Liste loeschen moechten.");
+	//char yesOrNo;
+	//scanf_s("%c", &yesOrNo);
+
+	//if (yesOrNo == 'j') {
+	//	deleteList(pStart);
+	//}
 	system("pause");
 }
