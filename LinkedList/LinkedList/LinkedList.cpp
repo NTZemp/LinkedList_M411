93cbc53a224f4ae059979a8e762c2313c1367149
linomeyer <linomeyer02@gmail.com>
2018-12-14 13:05:55 +0100

    add random generator

--- LinkedList/LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList/LinkedList.cpp
@@ -1,35 +1,51 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "time.h"
 
-typedef struct struElement {
+typedef struct Person {
 	char vorname[40];
 	char nachname[40];
 	int jahrgang;
-	struElement* pNext;
-} struElement;
+	struct Person* pNext;
+} struPerson;
 
-struElement* create(int amount) {
-	struElement* pStart = NULL;
-	struElement* pLast = NULL;
+char randomName() {
+	return rand() % 26 + 'A';
+}
+
+int randomYear() {
+	return 1900 + (rand() % 119);
+}
+
+struPerson* create(int amount) {
+	struPerson* pStart = NULL;
+	struPerson* pLast = NULL;
 
 	for (int i = 0; i < amount; i++) {
-		struElement* pNew = (struElement*)malloc(sizeof(struElement)); // malloc == new
+		struPerson* pNew = (struPerson*)malloc(sizeof(struPerson)); // malloc == new
 		pNew->pNext = NULL;
-		strcpy_s(pNew->vorname, "A");
-		strcpy_s(pNew->nachname, "B");
-		pNew->jahrgang = 1925;
+		pNew->vorname[0] = randomName();
+		pNew->vorname[1] = '\0';
+		pNew->nachname[0] = randomName();
+		pNew->nachname[1] = '\0';
+		pNew->jahrgang = randomYear();
 		if (pStart == NULL) pStart = pNew;
 		if (pLast != NULL) pLast->pNext = pNew;
 		pLast = pNew;
 	}
-
 	return pStart;
 }
 
-
+void printList(struPerson* pStart) {
+	for (struPerson* pOutput = pStart; pOutput != NULL; pOutput = pOutput->pNext) {
+		printf("Vorname: %c\nNachname: %c\nJahrgang: %d\n", pOutput->vorname[0], pOutput->nachname[0], pOutput->jahrgang);
+	}
+}
 
 void main() {
-	struElement* pStart = create(5);
+	srand((unsigned)time(NULL));
+	struPerson* pStart = create(5);
+	printList(pStart);
 	system("pause");
 }
